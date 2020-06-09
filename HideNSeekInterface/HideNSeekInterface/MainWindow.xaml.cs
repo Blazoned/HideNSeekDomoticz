051a4aae9b4ca81c78e29d5fa9e3875ea198aa96
- <bryankroon@ziggo.nl>
2020-06-09 13:28:55 +0200

    Namespaces Fixed

--- HideNSeekInterface/HideNSeekInterface/MainWindow.xaml.cs
+++ HideNSeekInterface/HideNSeekInterface/MainWindow.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace HideNSeekInterface
+namespace HideNSeek.Interface
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
