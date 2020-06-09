035b7e94d5186de82db8b4600ddc7e3c343ccf7f
- <bryankroon@ziggo.nl>
2020-06-09 12:49:12 +0200

    WPF Main

---
+++ HideNSeekInterface/HideNSeekInterface/LobbyWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace HideNSeekInterface
+{
+    /// <summary>
+    /// Interaction logic for LobbyWindow.xaml
+    /// </summary>
+    public partial class LobbyWindow : Window
+    {
+        public LobbyWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnClickDisconnect(object sender, RoutedEventArgs e)
+        {
+            Console.WriteLine("Lobby");
+            Window window = new MainWindow();
+            window.Show();
+            this.Close();
+        }
+    }
+}
